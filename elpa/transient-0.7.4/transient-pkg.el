e3e159e01cdc348ffc10b3e9f9fb87ecff5694ad
Asriyan Arthur <asriyanarthur@mail.ru>
2025-06-03 21:36:04 +0300

    fix

--- elpa/transient-0.7.4/transient-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from transient.el  -*- no-byte-compile: t -*-
-(define-package "transient" "0.7.4" "Transient commands" '((emacs "26.1") (compat "30.0.0.0") (seq "2.24")) :commit "b2cb4e578f2362a0354c4a31a6bd89d6c4b63d63" :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")) :maintainer '("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev") :keywords '("extensions") :url "https://github.com/magit/transient")
