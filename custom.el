ed3835044a48a79595cec2261263911d0900e47e
Asriyan Arthur <asriyanarthur@mail.ru>
2025-08-04 21:50:48 +0300

    ff

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/storage/downloads/all/org/home.org"))
  '(package-selected-packages
    '(counsel erlang flycheck go-mode helm helm-projectile
 	     ivy-erlang-complete lv magit markdown-mode
