30d1eb573fc892019b8df0394e7f03fb66132633
Asriyan Arthur <asriyanarthur@MacBook-Air-Asriyan.local>
2025-12-31 00:38:42 -0800

    add nord theme

--- custom.el
+++ custom.el
@@ -4,12 +4,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/storage/downloads/all/org/home.org"))
+ '(custom-enabled-themes '(nord))
+ '(custom-safe-themes
+   '("aa92c5258e1777df4c5a937ad0babbad1ac01f6381b87df971bfe48a1b905b25"
+     default))
+ '(org-agenda-files
+   '("~/all/org/tmp.org"
+     "/Users/asriyanarthur/storage/downloads/all/org/home.org"))
  '(package-selected-packages
    '(counsel erlang flycheck go-mode helm helm-projectile
 	     ivy-erlang-complete lv magit markdown-mode
-	     multiple-cursors nerd-icons-dired region-bindings-mode
-	     reverse-im))
+	     multiple-cursors nerd-icons-dired nord-theme
+	     region-bindings-mode reverse-im))
  '(package-vc-selected-packages
    '((helm :url "https://github.com/emacs-helm/helm.git" :branch "v4.0"))))
 (custom-set-faces
